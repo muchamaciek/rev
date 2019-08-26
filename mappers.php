5fbe3d06b3d36c2cc333c6d2e2ddb43bded55896
muchamaciek <muchamaciek@gmail.com>
2019-08-26 21:58:19 +0200

    encode/decode

--- mappers.php
+++ mappers.php
@@ -3,13 +3,13 @@
 // wersja z pojedyńczym czytaniem json
 
 $apiCall = ApiCallerFactory::getUsers();
-if($apiCall->makeCall()){ //makeCall encoduje json i zapisuje sobie do osobnego parametru dostepnego przez getResponseEncoded()
-    $user = UserMapper::getUser($apiCall->getResponseEncoded()); // 
+if($apiCall->makeCall()){ //makeCall decoduje json i zapisuje sobie do osobnego parametru dostepnego przez getResponseDecoded()
+    $user = UserMapper::getUser($apiCall->getResponseDecoded()); // 
 }
 
 // wersja z podwójnym czytaniem json
 
 $apiCall = ApiCallerFactory::getUsers();
 if($apiCall->makeCall()){
-    $user = UserMapper::getUser($apiCall->getResponse()); // do mappera przekazuję json w wersji string, który trzeba sobie jeszcze encodować.
+    $user = UserMapper::getUser($apiCall->getResponse()); // do mappera przekazuję json w wersji string, który trzeba sobie jeszcze decodować.
 }
