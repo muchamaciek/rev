ee5865161996c3fe867ed7ae48835e910d3d4819
muchamaciek <muchamaciek@gmail.com>
2019-08-26 22:15:27 +0200

    mappers

--- mappers.php
+++ mappers.php
@@ -13,3 +13,29 @@ $apiCall = ApiCallerFactory::getUsers();
 if($apiCall->makeCall()){
     $user = UserMapper::getUser($apiCall->getResponse()); // do mappera przekazuję json w wersji string, który trzeba sobie jeszcze decodować.
 }
+
+
+
+
+// wersja z parserem 
+
+$apiCall = ApiCallerFactory::getUsers();
+$apiResult = $apiCall->makeCall();  // obiekt klasy ApiResult jako return
+if(ApiResult->getStatus()){
+    $user = UserMapper::getUser($apiResult->getParsedResponse());
+    // inne rzeczy jeśli potrzebuje
+    $phpsesid = $apiResult->getPhpsessid();
+    $tocken = $apiResult->getTocken();
+}
+
+// wersja z parserem tworzonym ręcznie
+
+$apiCall = ApiCallerFactory::getUsers();
+$apiCall->makeCall();  
+$apiResult = new ApiResult($apiCall->getRawResponse());
+if(ApiResult->getStatus()){
+    $user = UserMapper::getUser($apiResult->getParsedResponse());
+    // inne rzeczy jeśli potrzebuje
+    $phpsesid = $apiResult->getPhpsessid();
+    $tocken = $apiResult->getTocken();
+}
